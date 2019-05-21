f88c857a20fa31b69d8f6e2ff60300ba51316a6f
Maruf <marufbinawal@gmail.com>
2019-05-21 14:27:35 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+class Student < User
 
+    attr_accessor :knowledge
+
+    @@all = []
+
+    def self.all
+      @@all
+    end
+
+        def initialize(knowledge)
+            @knowledge = knowledge
+            @@all << self  
+        end
+
+        def learn("")
+            knowledge.insert("")
+        end
+
+        def knowledge
+            Student.knowledge
+        end
 end
